0df5d0b759a7c5fe3d043d5ca5b22cfae396ffd6
JULIUSANGULU <74071966+JULIUSANGULU@users.noreply.github.com>
2026-02-08 01:23:13 +0100

    Feat: Installed hardhat and added prediction market smart contract

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv/config");
+
+module.exports = {
+    solidity: "0.8.20",
+    networks: {
+        baseSepolia: {
+            url: "https://sepolia.base.org",
+            accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+            chainId: 84532,
+        },
+        base: {
+            url: "https://mainnet.base.org",
+            accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+            chainId: 8453,
+        },
+    },
+};
